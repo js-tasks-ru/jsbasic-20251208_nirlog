0332c83e6f73f51abb3e9c03a2afc0863b14d8c2
Aleksandr Lebedev <nirlog@yandex.ru>
2026-01-08 20:42:21 +0300

    2-module full

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  sum = 0;
+  for (let key in salaries) {
+    if (typeof (salaries[key]) == "number" && Number.isFinite(salaries[key])) {
+      sum += salaries[key];
+    } 
+  } 
+  return sum;
 }
